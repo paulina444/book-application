f63ef1f257876752b058b8302c80aa2b93f2f21d
paulina444 <paulina.kaczmarek444111@gmail.com>
2024-12-27 19:41:53 +0100

    reviews srapper added

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from API import API_BOOKS
 from language_processing import Motives_list
+from scrapper_reviews import *
 
-title = "The Atlas Six"
-author = "Olivie Blake"
+# title = "The Atlas Six"
+# author = "Olivie Blake"
 
-title, author, motives, cover = API_BOOKS.get_book_data(title, author)
+# title, author, motives, cover = API_BOOKS.get_book_data(title, author)
 
-motives = Motives_list.m_list(motives)
-print(f"Tytuł: {title}")
-print(f"Autor: {author}")
-print(f"Motywy: {motives}")
+# motives = Motives_list.m_list(motives)
+# print(f"Tytuł: {title}")
+# print(f"Autor: {author}")
+# print(f"Motywy: {motives}")
+
+
+#####
+GoodReadsReviewsScrapper.scrape_user_reviews(185172573,'agata')
---
+++ scrapper_reviews.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+import csv
+
+class GoodReadsReviewsScrapper:
+    @staticmethod
+    def scrape_user_reviews(user_id, user_name):
+
+        chrome_options = Options()
+        #chrome_options.add_argument("--headless")  # nie wiem czemu to nie działa lol
+
+        service = Service(ChromeDriverManager().install()) # ChromeDriverManager().install() -pobranie wersji sterownika chromedriver dla google chrome # Service przekazanie lokalizacji sterownika do Selenium
+        driver = webdriver.Chrome(service=service, options=chrome_options) # uruchomienie instancji przegladarki
+
+        
+        user_profile_url = f'https://www.goodreads.com/user/show/{str(user_id)}-{user_name}'
+        driver.get(user_profile_url)
+
+        # załadowanie strony
+        time.sleep(5)
+
+        # znalezienie recenzji
+        reviews = driver.find_elements(By.XPATH, "//span[contains(@id, 'freeTextContainerreview')]") 
+
+        with open(user_name+'s_reviews.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            for review in reviews:
+                writer.writerow([review.text])
+
+        driver.quit()
