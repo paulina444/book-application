1dc7d0633e0549973cddce5f29eb215d93af25dc
Paulina Smierzchalska <pausmi3@st.amu.edu.pl>
2025-01-05 22:38:44 +0100

    remove class English_word

--- language_processing.py
+++ language_processing.py
@@ -1,12 +1,6 @@
 import nltk
 from nltk.corpus import words
 
-class English_word: #niezbyt to działa, ponieważ nie wykrywa np. że teenagers to angielskie słowo :/
-    #nltk.download('words') - odkomentować jeśli pierwszy raz jest wywoływane
-    def is_english_word(word):
-        english_words = set(words.words())
-        return word.lower() in english_words
-
 class Motives_list:
     def m_list(motives):
         for i in range(len(motives)):
